53d3704809cc80e3e5190af06d82138cd3b1d9cf
elmaxe <axel.elmarsson@gmail.com>
2021-11-03 12:23:23 +0100

    Populate landing page with info

--- client/src/views/Landing/Landing.tsx
+++ client/src/views/Landing/Landing.tsx
@@ -52,13 +52,23 @@ export const Landing: React.FC = (props) => {
             </Pick>
             <div id="content" style={{minHeight: 0}}>
                 <h2>Damm</h2>
-                <p>Löksås ipsum ta är samtidigt så vidsträckt denna rännil plats, samma färdväg strand söka i där gör. Sig hans dimma strand dock sällan tid åker hans, enligt lax tiden kom smultron åker dock dag vad, göras mjuka där annan redan för hwila. Därmed ser sjö dag brunsås träutensilierna har bra, det att redan det oss och mjuka inom, ska nu år tidigare hav så.</p>
+                <p>Välkommen till Damm - Konglig Datasektionens historiesystem.</p>
+                <p>
+                    Detta system sammanställer sektionens historia på tre sätt: märkesarkivet, tidslinjen och museet.
+                </p>
+                <p>Damm hanteras av sektionshistorikern, vänd dig till hen om du har några frågor eller vill bidra med historia: <a href="mailto:historiker@d.kth.se" target="_blank" rel="noopener noreferrer">historiker@d.kth.se</a></p>
                 <h3>Märkesarkivet</h3>
-                <p>Löksås ipsum ta är samtidigt så vidsträckt denna rännil plats</p>
-                <h3>Museum</h3>
-                <p>Löksås ipsum ta är samtidigt så vidsträckt denna rännil plats</p>
+                <p>Märkesarkivet arkiverar märken som skapats inom sektionen genom tiderna. Här finner du allt från årskursmärket 1991 till fidgetspinnermärken från 2017. Arkivet består egentligen av ett fysiskt arkiv och ett digitalt arkiv. I det fysiska arkivet sparas märken för att sedan fotograferas och läggas upp i Damm. Damm, det digitala arkivet, är ett sätt för sektionsmedlemmar att lätt få tillgång till märkesarkivet. Arkivet är beroende av att funktionärer, projekt och evenemang sparar ett exemplar av varje märke som skapas och donerar till arkivet.</p>
                 <h3>Tidslinjen</h3>
-                <p>Löksås ipsum ta är samtidigt så vidsträckt denna rännil plats</p>
+                <p>Tidslinjen visar viktiga historiska händelser som skett i sektionen sedan dess grundande den 7 oktober 1983.</p>
+                <h3>Museum</h3>
+                <p>Museet är tänkt att visa upp "artefakter" och viktiga föremål som tillhör sektionen.</p>
+                <h2>Hittat en bugg? Vill du bidra?</h2>
+                <p>
+                    Damm finns på <a href="https://github.com/datasektionen/damm2" target="_blank" rel="noopener noreferrer">GitHub</a>
+                </p>
+                <h2>Damms historia</h2>
+                <p>Jonas Dahl skrev tidslinjen under 2017. Axel Elmarsson skrev sedan resterande systemet 2020 och 2021.</p>
             </div>
         </StyledLanding>
     )
