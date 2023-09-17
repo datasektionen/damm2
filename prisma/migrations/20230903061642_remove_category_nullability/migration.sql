0e6b408b0debf05dedfc89fad71e03ca381d4db8
Axel Elmarsson <axel.elmarsson@gmail.com>
2023-09-17 17:03:53 +0200

    fix: remove migration

--- prisma/migrations/20230903061642_remove_category_nullability/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `category` on table `Tag` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "Tag" ALTER COLUMN "category" SET NOT NULL;
