c8357034031b17a5d1de7d87949857296e9d1c79
Axel Elmarsson <axel.elmarsson@gmail.com>
2023-09-17 16:45:03 +0200

    fix: add easter eggs

--- client/src/views/DarkMode/DarkMode.tsx
+++ client/src/views/DarkMode/DarkMode.tsx
@@ -6,38 +6,72 @@ import { useDarkMode } from '../../hooks/useDarkMode';
 type DarkModeProps = {};
 
 export const DarkMode: FC<DarkModeProps> = memo((props) => {
+  const [value, setValue] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [darkmodeAudio, setDarkmodeAudio] = useState(
+    new Audio('/nocturne.ogg')
+  );
+  const [lightmodeAudio, setLightmodeAudio] = useState(
+    new Audio('/keeper_of_the_light.mp3')
+  );
+  const [playing, setPlaying] = useState(false);
 
-    const [value, setValue] = useState(false);
-    const [loading, setLoading] = useState(false);
+  const { refetch } = useDarkMode();
 
-    const { refetch } = useDarkMode();
+  const getValue = useCallback(() => {
+    axios.get('/api/dark-mode').then((res) => {
+      setValue(res.data);
+    });
+  }, []);
 
-    const getValue = useCallback(() => {
-        axios.get("/api/dark-mode")
-            .then(res => {
-                setValue(res.data)
-            })
-    }, [])
+  useEffect(() => {
+    darkmodeAudio.addEventListener('ended', () => setPlaying(false));
+    return () => {
+      darkmodeAudio.removeEventListener('ended', () => setPlaying(false));
+    };
+  }, []);
 
-    const onChange = useCallback((v: string) => {
-        setLoading(true);
-        axios.patch("/api/dark-mode")
-            .then(res => {
-                setValue(res.data)
-            })
-            .finally(() => {
-                refetch();
-                setLoading(false);
-            })
-    }, [])
+  useEffect(() => {
+    lightmodeAudio.addEventListener('ended', () => setPlaying(false));
+    return () => {
+      lightmodeAudio.removeEventListener('ended', () => setPlaying(false));
+    };
+  }, []);
 
-    useEffect(getValue , []);
+  const onChange = useCallback((v: string) => {
+    setLoading(true);
+    axios
+      .patch('/api/dark-mode')
+      .then((res) => {
+        setValue(res.data);
+        if (res.data === true) {
+          setPlaying(true);
+          darkmodeAudio.volume = 0.3;
+          darkmodeAudio.play();
+        } else if (res.data === false) {
+          lightmodeAudio.volume = 0.3;
+          lightmodeAudio.play();
+        }
+      })
+      .finally(() => {
+        refetch();
+        setLoading(false);
+      });
+  }, []);
 
-    return (
-        <div>
-            <h2>Mörkläggning</h2>
-            <p>Mörkläggningsläget döljer tidslinjen och mottagningsmärken.</p>
-            <BiSwitch left={{ key: "off",  label: "AV"}} right={{ key: "on",  label: "PÅ"}} setValue={onChange} value={value ? "on" : "off"} disabled={loading} />
-        </div>
-    )
+  useEffect(getValue, []);
+
+  return (
+    <div>
+      <h2>Mörkläggning</h2>
+      <p>Mörkläggningsläget döljer tidslinjen och mottagningsmärken.</p>
+      <BiSwitch
+        left={{ key: 'off', label: 'AV' }}
+        right={{ key: 'on', label: 'PÅ' }}
+        setValue={onChange}
+        value={value ? 'on' : 'off'}
+        disabled={loading}
+      />
+    </div>
+  );
 });
